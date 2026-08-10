c3b23300be4a9f742653d33f184a1f2ac0aaaa23
Jensy Jimenez <jensyjimenez723@gmail.com>
2026-08-10 10:18:57 -0400

    Export AI carrier control

--- functions/v2-index.js
+++ functions/v2-index.js
@@ -7,4 +7,5 @@ export * from './hybrid-dialer-api.js';
 export * from './hybrid-session-api.js';
 export * from './hybrid-ai-sip-dispatch.js';
 export * from './hybrid-sideband-control.js';
+export * from './hybrid-ai-carrier-control.js';
 export * from './hybrid-voicemail.js';
